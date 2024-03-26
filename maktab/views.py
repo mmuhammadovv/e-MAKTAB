b729a2239c13cf951cd5b202cb0903f963df7b22
ali <mmuhammadovv@gmail.com>
2024-03-26 15:30:20 +0500

    Stili tayyor bo'ldi

--- maktab/views.py
+++ maktab/views.py
@@ -4,3 +4,33 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def admin_account(request):
+    return render(request, 'admin_account.html')
+
+def teacher_account(request):
+    return render(request, 'teacher_account.html')
+
+def pupil_account(request):
+    return render(request, 'pupil_account.html')
+
+def pupils(request):
+    return render(request, 'pupils.html')
+
+def teachers(request):
+    return render(request, 'teachers.html')
+
+def lessons(request):
+    return render(request, 'lessons.html')
+
+
+def add_lesson(request):
+    return render(request, 'add_lesson.html')
+
+
+def add_teacher(request):
+    return render(request, 'add_teacher.html')
+
+
+def add_pupil(request):
+    return render(request, 'add_pupil.html')
